da428eb27d0c9134589badbd12585d937190b7c5
mhimavamshi <himavamshi1212@gmail.com>
2024-10-07 14:58:59 +0530

    correct rectangle out of bounds

--- demos/rectangles.py
+++ demos/rectangles.py
@@ -14,15 +14,24 @@ WIDTH, HEIGHT = 1000, 1000
 pygame.display.set_caption("Rectangles")
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 BACKGROUND_COLOR = "black"
+MASS_PER_AREA = 3
+NUMBER_OF_RECTS = 10
+
 
 clock = pygame.time.Clock()
 
 physics_engine = PhysicsEngine(x_limit = WIDTH, y_limit = HEIGHT) # TODO: provide configs like what collider to use, etc etc
 delta_time = 0
 
-rectangle = Rectangle(Vector(WIDTH / 2, HEIGHT / 2), 5, (50, 50), "white")
-rectangle.apply_force(Vector(10, 10).scale(5))
-physics_engine.register(rectangle)
+rectangles = []
+for i in range(NUMBER_OF_RECTS):
+    RECT_WIDTH = random.randint(3, 5)
+    RECT_HEIGHT = random.randint(4, 8)
+    area = RECT_WIDTH * RECT_HEIGHT
+    mass = MASS_PER_AREA * area
+    rectangle = Rectangle(RandomVector((RECT_WIDTH + 1, WIDTH - RECT_WIDTH * 2), (RECT_HEIGHT + 1, HEIGHT - RECT_HEIGHT * 2)), 5, (50, 50), "white")
+    rectangle.apply_force(RandomVector((-5, 10),  (-10, 5)).scale(mass))
+    physics_engine.register(rectangle)
 
 # game loop
 running = True 
