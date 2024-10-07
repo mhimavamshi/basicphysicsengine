0ec0c1f59cb79cd9ecbe8df8d69ee1aefdb3cb9a
mhimavamshi <himavamshi1212@gmail.com>
2024-10-07 15:04:44 +0530

    added rectangles

--- demos/rectangles.py
+++ demos/rectangles.py
@@ -5,7 +5,6 @@ import pygame
 from src.physicsengine import *
 from src.vector import RandomVector
 import random
-import math
 
 pygame.init()
 
@@ -15,7 +14,7 @@ pygame.display.set_caption("Rectangles")
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 BACKGROUND_COLOR = "black"
 MASS_PER_AREA = 3
-NUMBER_OF_RECTS = 10
+NUMBER_OF_RECTS = 45
 
 
 clock = pygame.time.Clock()
@@ -23,13 +22,15 @@ clock = pygame.time.Clock()
 physics_engine = PhysicsEngine(x_limit = WIDTH, y_limit = HEIGHT) # TODO: provide configs like what collider to use, etc etc
 delta_time = 0
 
+color_list = list(pygame.color.THECOLORS.keys())
+color_list.remove(BACKGROUND_COLOR)
 rectangles = []
 for i in range(NUMBER_OF_RECTS):
     RECT_WIDTH = random.randint(3, 5)
     RECT_HEIGHT = random.randint(4, 8)
     area = RECT_WIDTH * RECT_HEIGHT
     mass = MASS_PER_AREA * area
-    rectangle = Rectangle(RandomVector((RECT_WIDTH + 1, WIDTH - RECT_WIDTH * 2), (RECT_HEIGHT + 1, HEIGHT - RECT_HEIGHT * 2)), 5, (50, 50), "white")
+    rectangle = Rectangle(Vector(WIDTH / 2, HEIGHT / 2), 5, (50, 50), random.choice(color_list))
     rectangle.apply_force(RandomVector((-5, 10),  (-10, 5)).scale(mass))
     physics_engine.register(rectangle)
 
